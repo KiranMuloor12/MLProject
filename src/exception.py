a16356b95bdf3813ce6f3623a2575e44e4d846cc
KiranMuloor12 <kiranchitra0912@hotmail.com>
2023-03-29 06:46:19 +0530

    data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -21,10 +21,5 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Division by zero")
-        raise CustomException(e, sys)
+
 
